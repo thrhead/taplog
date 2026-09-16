aeebf771fbc63042a30f1ffa56aa6183d772f0dd
Tahir <122523449+thrhead@users.noreply.github.com>
2026-09-16 05:32:46 +0000

    test: add JVM test scaffolds for core and data modules

---
+++ data/src/test/kotlin/io/github/thrhead/taplog/data/DataTestScaffoldTest.kt
@@ -0,0 +1,11 @@
+package io.github.thrhead.taplog.data
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class DataTestScaffoldTest {
+    @Test
+    fun dataTestLaneIsOperational() {
+        assertTrue("Data JVM test scaffold should run", true)
+    }
+}
